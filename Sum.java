020ae938b9b1a111a64c3eef1901a5bd08f636a2
Anjali <43513233+vinisharen@users.noreply.github.com>
2020-06-04 20:45:03 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class sum {
+
+    
+  public static void main(String[] args)
+
+{       
+    int i,n=0,sum=0;
+	{
+	   
+        System.out.println("Input the 3 numbers : ");  
+         
+	}
+		for (i=0;i<3;i++)
+		{
+		    Scanner in = new Scanner(System.in);
+		    n = in.nextInt();
+		    
+  		sum +=n;
+	}
+	System.out.println("The sum of 3 no is : " +sum+"\n);
+ 
+}
+}
