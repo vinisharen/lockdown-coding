84bf1e758739820770893ea8526fed0015edb18d
Anjali <43513233+vinisharen@users.noreply.github.com>
2020-06-11 13:29:59 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class SegregateOddEven
+{
+    static void segregateEvenOdd(int arr[])
+    {
+        int left = 0, right = arr.length - 1;
+        while (left < right)
+        {
+            while (arr[left]%2 == 0 && left < right)
+            left++;
+            while (arr[right]%2 == 1 && left < right)
+            right--;
+            if (left < right)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+public static void main (String[] args)
+{
+    int arr[] = {12, 34, 45, 9, 8, 90, 3};
+    segregateEvenOdd(arr);
+    System.out.print("Array after segregation ");
+    for (int i = 0; i < arr.length; i++)
+    System.out.print(arr[i]+" ");
+    }
+} 
